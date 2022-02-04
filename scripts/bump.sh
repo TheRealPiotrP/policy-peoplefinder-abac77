f50dcb4d48538b671e106506d5096f2bfe913940
Piotr Puszkiewicz <piotr@aserto.com>
2022-02-04 06:43:45 +0000

    0.0.1-dirty

---
+++ scripts/bump.sh
@@ -0,0 +1,9 @@
+for i in {1..30}
+do
+  echo "# `sver -n patch`" >> ./src/policies/get.rego
+  git add .
+  git commit -m "`sver -n patch`"
+  git tag `sver -n patch`
+  git push
+  git push --tags
+done
